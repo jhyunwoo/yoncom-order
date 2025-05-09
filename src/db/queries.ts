88dd5df231c8703e8d52b6fc84811b3b434f7613
최호영 <Croksuter@github.com>
2025-05-09 18:26:15 +0900

    adjust query Function with option object

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -5,21 +5,25 @@ import { sql, and, eq, isNull, inArray, asc, desc } from "drizzle-orm";
 export const queryUsers = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   userIds: string[],
-  withTables: boolean = false,
-  withMenuCategories: boolean = false,
-  includeDeleted: boolean = false,
+  option: {
+    withTables?: true,
+    withMenuCategories?: true,
+    includeDeleted?: true,
+  }
 ): Promise<(typeof Schema.users.$inferSelect & {
   tables: typeof Schema.tables.$inferSelect[] | string[],
   menuCategories: typeof Schema.menuCategories.$inferSelect[] | string[],
 })[]> => {
+  const { withTables, withMenuCategories, includeDeleted } = option;
+
   return await db.query.users.findMany({ 
     where: and(
       inArray(Schema.users.id, userIds), 
       includeDeleted ? undefined : isNull(Schema.users.deletedAt)
     ),  
     with: {
-      tables: withTables ? true : undefined,
-      menuCategories: withMenuCategories ? true : undefined,
+      tables: withTables,
+      menuCategories: withMenuCategories,
     }
   });
 }
@@ -27,18 +31,22 @@ export const queryUsers = async (
 export const queryMenuCategories = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   menuCategoryIds: string[],
-  withUser: boolean = false,
-  withMenus: boolean = false,
-  includeDeleted: boolean = false,
+  option: {
+    withUser?: true,
+    withMenus?: true,
+    includeDeleted?: true,
+  }
 ) => {
+  const { withUser, withMenus, includeDeleted } = option;
+
   return await db.query.menuCategories.findMany({
     where: and(
       inArray(Schema.menuCategories.id, menuCategoryIds), 
       includeDeleted ? undefined : isNull(Schema.menuCategories.deletedAt)
     ),
     with: {
-      user: withUser ? true : undefined,
-      menus: withMenus ? true : undefined,
+      user: withUser,
+      menus: withMenus,
     }
   });
 }
@@ -46,18 +54,22 @@ export const queryMenuCategories = async (
 export const queryMenus = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   menuIds: string[],
-  withMenuCategory: boolean = false,
-  withMenuOrders: boolean = false,
-  includeDeleted: boolean = false,
+  option: {
+    withMenuCategory?: true,
+    withMenuOrders?: true,
+    includeDeleted?: true,
+  }
 ) => {
+  const { withMenuCategory, withMenuOrders, includeDeleted } = option;
+
   return await db.query.menus.findMany({
     where: and(
       inArray(Schema.menus.id, menuIds),
       includeDeleted ? undefined : isNull(Schema.menus.deletedAt)
     ),
     with: {
-      menuCategory: withMenuCategory ? true : undefined,
-      menuOrders: withMenuOrders ? true : undefined,
+      menuCategory: withMenuCategory,
+      menuOrders: withMenuOrders,
     }
   });
 }
@@ -65,18 +77,22 @@ export const queryMenus = async (
 export const queryTables = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   tableIds: string[],
-  withUser: boolean = false,
-  withTableContexts: boolean = false,
-  includeDeleted: boolean = false,  
+  option: {
+    withUser?: true,
+    withTableContexts?: true,
+    includeDeleted?: true,
+  }
 ) => {
+  const { withUser, withTableContexts, includeDeleted } = option;
+
   return await db.query.tables.findMany({
     where: and(
       inArray(Schema.tables.id, tableIds), 
       includeDeleted ? undefined : isNull(Schema.tables.deletedAt)
     ),
     with: {
-      user: withUser ? true : undefined,
-      tableContexts: withTableContexts ? true : undefined,
+      user: withUser,
+      tableContexts: withTableContexts,
     }
   });
 }
@@ -84,23 +100,27 @@ export const queryTables = async (
 export const queryTableContexts = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   tableContextIds: string[],
-  withTable: boolean = false,
-  withOrders: boolean = false,
-  onlyActive: boolean = false,
-  order: "asc" | "desc" = "desc",
+  option: {
+    withTable?: true,
+    withOrders?: true,
+    onlyActive?: true,
+    order?: "asc",
+  }
 ): Promise<(typeof Schema.tableContexts.$inferSelect & {
   table: typeof Schema.tables.$inferSelect | string,
   orders: typeof Schema.orders.$inferSelect[] | string[],
 })[]> => {
+  const { withTable, withOrders, onlyActive, order } = option;
+
   return await db.query.tableContexts.findMany({
     where: and(
       inArray(Schema.tableContexts.id, tableContextIds),
       onlyActive ? isNull(Schema.tableContexts.deletedAt) : undefined,
-      order === "asc" ? asc(Schema.tableContexts.createdAt) : desc(Schema.tableContexts.createdAt)
+      order ? asc(Schema.tableContexts.createdAt) : desc(Schema.tableContexts.createdAt)
     ),
     with: {
-      table: withTable ? true : undefined,
-      orders: withOrders ? true : undefined,
+      table: withTable,
+      orders: withOrders,
     },
   });
 }
@@ -108,41 +128,61 @@ export const queryTableContexts = async (
 export const queryOrders = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   orderIds: string[],
-  withTableContext: boolean = false,
-  withMenuOrders: boolean = false,
-  onlyActive: boolean = false,
-  order: "asc" | "desc" = "desc",
+  option: {
+    withTableContext?: true,
+    withMenuOrders?: true,
+    onlyActive?: true,
+    order?: "asc",
+  }
 ) => {
+  const { withTableContext, withMenuOrders, onlyActive, order } = option;
+
   const orders = await db.query.orders.findMany({
     where: inArray(Schema.orders.id, orderIds),
     with: {
-      tableContext: withTableContext ? true : undefined,
-      menuOrders: withMenuOrders ? true : undefined,
+      tableContext: withTableContext,
+      menuOrders: (withMenuOrders || onlyActive) ? true : undefined,
     },
-    orderBy: order === "asc" ? asc(Schema.orders.createdAt) : desc(Schema.orders.createdAt),
+    orderBy: order ? asc(Schema.orders.createdAt) : desc(Schema.orders.createdAt),
   });
 
-  if (!onlyActive) return orders
+  return orders.filter((order) => {
+    return order.menuOrders.some((menuOrder) => {
+      return onlyActive
+        ? menuOrder.status === Schema.menuOrderStatus.PENDING
+        : true
+    })
+  }).map((order) => {
+    return withMenuOrders
+      ? order
+      : {
+        ...order,
+        menuOrders: order.menuOrders.map((menuOrder) => menuOrder.id)
+      } 
+  })
 }
 
 export const queryMenuOrders = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   menuOrderIds: string[],
-  withOrder: boolean = false,
-  withMenu: boolean = false,
-  onlyActive: boolean = false,
+  option: {
+    withOrder?: true,
+    withMenu?: true,
+    onlyActive?: true,
+  }
 ): Promise<(typeof Schema.menuOrders.$inferSelect & {
   order: typeof Schema.orders.$inferSelect | string,
   menu: typeof Schema.menus.$inferSelect | string,
 })[]> => {
+  const { withOrder, withMenu, onlyActive } = option;
   return await db.query.menuOrders.findMany({
     where: and(
       inArray(Schema.menuOrders.id, menuOrderIds),
       onlyActive ? eq(Schema.menuOrders.status, Schema.menuOrderStatus.PENDING) : undefined,
     ),
     with: {
-      order: withOrder ? true : undefined,
-      menu: withMenu ? true : undefined,
+      order: withOrder,
+      menu: withMenu,
     },
   });
 }
@@ -152,7 +192,7 @@ export const isMenusOwnedByMenuCategory = async (
   menuCategoryId: string,
   menuIds: string[],
 ) => {
-  const menus = await queryMenus(db, menuIds, true);
+  const menus = await queryMenus(db, menuIds, { withMenuCategory: true });
   return !menus.some((menu) => menu.menuCategoryId !== menuCategoryId)
 }
 
@@ -161,7 +201,7 @@ export const isMenuCategoriesOwnedByUser = async (
   userId: string,
   menuCategoryIds: string[],
 ) => {
-  const menuCategories = await queryMenuCategories(db, menuCategoryIds, true, false);
+  const menuCategories = await queryMenuCategories(db, menuCategoryIds, {});
   return !menuCategories.some((menuCategory) => menuCategory.userId !== userId)
 }
 
@@ -170,9 +210,9 @@ export const isMenusOwnedByUser = async (
   userId: string,
   menuIds: string[],
 ) => {
-  const menus = await queryMenus(db, menuIds);
+  const menus = await queryMenus(db, menuIds, {});
   const menuCategoryIds = menus.map((menu) => menu.menuCategoryId);
-  const menuCategories = await queryMenuCategories(db, menuCategoryIds, true, false);
+  const menuCategories = await queryMenuCategories(db, menuCategoryIds, {});
   return !menuCategories.some((menuCategory) => menuCategory.userId !== userId)
 }
 
@@ -181,7 +221,7 @@ export const isTablesOwnedByUser = async (
   userId: string,
   tableIds: string[],
 ) => {
-  const tables = await queryTables(db, tableIds);
+  const tables = await queryTables(db, tableIds, {});
   return !tables.some((table) => table.userId !== userId)
 }
 
@@ -189,7 +229,7 @@ export const isTableContextsActive = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   tableContextIds: string[],
 ) => {
-  const tableContexts = await queryTableContexts(db, tableContextIds, false, false, true);
+  const tableContexts = await queryTableContexts(db, tableContextIds, { onlyActive: true });
   return tableContexts.length > 0;
 }
 
@@ -197,17 +237,25 @@ export const isTablesActive = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   tableIds: string[],
 ) => {
-  const tables = await queryTables(db, tableIds, false, false, false);
+  const tables = await queryTables(db, tableIds, {});
   return tables.some(async (table) => {
-    const recentTableContext = await queryTableContexts(db, [table.tableContexts[0].id], false, false, true);
+    const recentTableContext = await queryTableContexts(db, [table.tableContexts[0].id], { onlyActive: true });
     return recentTableContext.length > 0;
   });
+}
+
+export const isMenuOrdersActive = async (
+  db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
+  menuOrderIds: string[],
+) => {
+  const menuOrders = await queryMenuOrders(db, menuOrderIds, {});
+  return menuOrders.some((menuOrder) => menuOrder.status !== Schema.menuOrderStatus.CANCELLED);
 }
 
 export const isOrdersActive = async (
   db: DrizzleD1Database<typeof import("~/db/schema")> & { $client: D1Database; },
   orderIds: string[],
 ) => {
-  const orders = await queryOrders(db, orderIds, false, false, false, true);
-  return orders.some((order) => order.status !== Schema.orderStatus.CANCELLED);
+  const orders = await queryOrders(db, orderIds, {});
+  return orders.some((order) => isMenuOrdersActive(db, order.menuOrders as string[]));
 }
