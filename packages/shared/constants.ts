dac1ac43bd84fe12c315879636a1d6dffffbd45b
jhyunwoo <jhyunwoo0228@gmail.com>
2025-05-28 01:59:48 +0900

    fix url

--- packages/shared/constants.ts
+++ packages/shared/constants.ts
@@ -1,8 +1,10 @@
-export const DEV_API_BASE_URL = "http://localhost:8787/api";
-export const PROD_API_BASE_URL = "<PROD_API_BASE_URL>";
+export const DEV_API_BASE_URL =
+  "https://yoncom-order-api.moveto.workers.dev/api";
+export const PROD_API_BASE_URL =
+  "https://yoncom-order-api.moveto.workers.dev/api";
 
-export const DEV_WEB_BASE_URL = "http://localhost:5173";
-export const PROD_WEB_BASE_URL = "<PROD_WEB_BASE_URL>";
+export const DEV_WEB_BASE_URL = "https://yoncomorder.moveto.kr";
+export const PROD_WEB_BASE_URL = "https://yoncomorder.moveto.kr";
 
 export const API_BASE_URL = DEV_API_BASE_URL;
 export const WEB_BASE_URL = DEV_WEB_BASE_URL;
