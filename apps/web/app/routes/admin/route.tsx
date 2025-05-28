e4a1d5eaa882dcc678501422b829c0f9c858b21c
최호영 <Croksuter@github.com>
2025-05-28 11:20:29 +0900

    change authorization logic

--- apps/web/app/routes/admin/route.tsx
+++ apps/web/app/routes/admin/route.tsx
@@ -1,21 +1,30 @@
 // app/routes/admin.tsx
 import { Outlet, redirect } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
-import { requireUser } from "~/lib/auth";
+import { isSignedIn, requireUser } from "~/lib/auth";
 import { useEffect } from "react";
 
 import useMenuStore from "~/stores/menu.store";
 import useTableStore from "~/stores/table.store";
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const user = await requireUser(request); // API 통해 인증
-  if (!user) throw redirect("/auth");
+// export const loader: LoaderFunction = async ({ request }) => {
+//   const user = await requireUser(request); // API 통해 인증
+//   if (!user) throw redirect("/auth");
   
-  return user;
-};
+//   return user;
+// };
 
 export default function AdminLayout() {
   useEffect(() => {
+    isSignedIn((res) => {
+      if (res.user === null) {
+        window.location.href = "/auth";
+      }
+    }, (error) => {
+      console.error(error);
+      window.location.href = "/auth";
+    });
+
     const interval = setInterval(async () => {
       await useMenuStore.getState().adminLoad({});
       await useTableStore.getState().load({});
